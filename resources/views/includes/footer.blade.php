3af903e9e0a76b37cedb2289f1cfc896951979e5
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-10-31 18:36:12 +0100

    footer view

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Strona główna</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Przepisy</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">O nas</a></li>
+        <li class="nav-item"><a href="/login" class="nav-link px-2 text-body-secondary">Zaloguj się</a></li>
+        <li class="nav-item"><a href="/register" class="nav-link px-2 text-body-secondary">Zarejestruj się</a></li>
+        <li class="nav-item"><a href="/userPanel" class="nav-link px-2 text-body-secondary"> Twoje konto</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2023 Foodie fix, Inc</p>
+</footer>
+
+<style>
+    footer {
+        box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+        position: relative;
+        z-index: 2;
+    }
+</style>
