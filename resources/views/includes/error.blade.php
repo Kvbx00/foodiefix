c4e39abf2d9dbf2c87b1352fba654c61ca60fe2b
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-11-02 10:27:32 +0100

    error view fixed

--- resources/views/includes/error.blade.php
+++ resources/views/includes/error.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
     <div class="alert alert-warning alert-dismissible fade show fixed-top d-flex justify-content-center" role="alert">
         @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+            <li class="me-4">{{ $error }}</li>
         @endforeach
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
