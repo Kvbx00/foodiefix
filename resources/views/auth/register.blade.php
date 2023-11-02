5fb740a86c75d56f038024f046cdf226ac6e3994
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-11-02 10:26:44 +0100

    sign-up page view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,104 +1,183 @@
 <!doctype html>
-<html>
+<html lang="pl">
 
-<head>
-    <meta charset="UTF-8">
-    <title>FoodieFix</title>
-</head>
+@include('includes.head')
 
 <body>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
 
-        <div class="form-group">
-            <label for="name">Imię</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" required autofocus>
+<div class="container-fluid h-100" style="background-color: #d8f3dc;">
+
+    @include('includes.error')
+
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+                <div class="card my-4" style="border-radius: 1rem;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 d-none d-xl-block" id="form-image" style="border-radius: 1rem 0 0 1rem;">
+                        </div>
+                        <div class="col-xl-6">
+                            <div class="card-body p-md-5 text-black">
+                                <div class="d-flex align-items-center mb-3 pb-1">
+                                    <img src="{{ asset('images/logo.png') }}" alt="logo" width="50" height="44">
+                                </div>
+                                <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Zarejestruj się</h5>
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-floating">
+                                                <input type="text" id="floatingName"
+                                                       class="form-control form-control-lg" placeholder="" name="name"
+                                                       value="{{ old('name') }}" required autofocus/>
+                                                <label class="form-label" for="floatingName">Imię</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-floating">
+                                                <input type="text" id="floatingLastName"
+                                                       class="form-control form-control-lg" placeholder=""
+                                                       name="lastName"
+                                                       value="{{ old('lastName') }}" required/>
+                                                <label class="form-label" for="floatingLastName">Nazwisko</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
+                                        <div class="form-check form-check-inline mb-0 me-4">
+                                            <input class="form-check-input" type="radio" name="gender"
+                                                   id="femaleGender"
+                                                   value="Kobieta" checked/>
+                                            <label class="form-check-label" for="femaleGender">Kobieta</label>
+                                        </div>
+                                        <div class="form-check form-check-inline mb-0 me-4">
+                                            <input class="form-check-input" type="radio" name="gender"
+                                                   id="maleGender"
+                                                   value="Mężczyzna"/>
+                                            <label class="form-check-label" for="maleGender">Mężczyzna</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <select class="form-select" name="physicalActivity" id="physicalActivity"
+                                                    required>
+                                                <option value="" selected disabled hidden>Poziom Aktywności</option>
+                                                <option {{ old('physicalActivity') == 'Brak treningów' ? 'selected' : '' }}>
+                                                    Brak treningów
+                                                </option>
+                                                <option {{ old('physicalActivity') == 'Niska aktywność' ? 'selected' : '' }}>
+                                                    Niska aktywność
+                                                </option>
+                                                <option {{ old('physicalActivity') == 'Średnia aktywność' ? 'selected' : '' }}>
+                                                    Średnia aktywność
+                                                </option>
+                                                <option {{ old('physicalActivity') == 'Wysoka aktywność' ? 'selected' : '' }}>
+                                                    Wysoka aktywność
+                                                </option>
+                                                <option {{ old('physicalActivity') == 'Bardzo wysoka aktywność' ? 'selected' : '' }}>
+                                                    Bardzo wysoka aktywność
+                                                </option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <select class="form-select" name="goal" id="goal">
+                                                <option value="" selected disabled hidden>Cel diety</option>
+                                                <option {{ old('goal') == 'Chcę schudnąć' ? 'selected' : '' }}>Chcę
+                                                    schudnąć
+                                                </option>
+                                                <option {{ old('goal') == 'Chcę utrzymać wagę' ? 'selected' : '' }}>Chcę
+                                                    utrzymać wagę
+                                                </option>
+                                                <option {{ old('goal') == 'Chcę przytyć' ? 'selected' : '' }}>Chcę
+                                                    przytyć
+                                                </option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4 mb-4">
+                                            <select class="form-select" name="age" id="age">
+                                                <option value="" selected disabled hidden>Wiek</option>
+                                                @for ($i = 13; $i <= 99; $i++)
+                                                    <option
+                                                        value="{{ $i }}" {{ old('age') == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4 mb-4">
+                                            <select class="form-select" name="height" id="height">
+                                                <option value="" selected disabled hidden>Wzrost</option>
+                                                @for ($i = 100; $i <= 220; $i++)
+                                                    <option
+                                                        value="{{ $i }}" {{ old('height') == $i ? 'selected' : '' }}>{{ $i }}
+                                                        cm
+                                                    </option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4 mb-4">
+                                            <select class="form-select" name="weight" id="weight">
+                                                <option value="" selected disabled hidden>Waga</option>
+                                                @for ($i = 30; $i <= 200; $i++)
+                                                    <option
+                                                        value="{{ $i }}" {{ old('weight') == $i ? 'selected' : '' }}>{{ $i }}
+                                                        kg
+                                                    </option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-floating mb-4">
+                                        <input type="email" class="form-control"
+                                               id="floatingEmail"
+                                               placeholder="" name="email" value="{{ old('email') }}" required>
+                                        <label for="floatingEmail">Adres email</label>
+                                    </div>
+
+                                    <div class="form-floating mb-4">
+                                        <input type="password"
+                                               class="form-control"
+                                               id="floatingPassword"
+                                               placeholder="" name="password" required>
+                                        <label for="floatingPassword">Hasło</label>
+                                    </div>
+
+                                    <div class="form-floating mb-4">
+                                        <input type="password"
+                                               class="form-control"
+                                               id="floatingPassword"
+                                               placeholder="" name="password_confirmation" required>
+                                        <label for="floatingPassword">Powtórz hasło</label>
+                                    </div>
+
+                                    <div class="d-flex justify-content-end pt-3">
+                                        <button class="btn btn-dark btn-lg btn-block" type="submit">Zarejestruj</button>
+                                    </div>
+                                    <p class="" style="color: #393f81;">Masz już konto?
+                                        <a href="/login" style="color: #393f81;">Zaloguj się</a></p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+</div>
 
-        <div class="form-group">
-            <label for="lastName">Nazwisko</label>
-            <input type="text" id="lastName" name="lastName" value="{{ old('lastName') }}" required>
-        </div>
-
-        <div class="form-group">
-            <label for="gender">Płeć</label>
-            <select name="gender" id="gender">
-                <option value="Mężczyzna" {{ old('gender') == 'Mężczyzna' ? 'selected' : '' }}>Mężczyzna</option>
-                <option value="Kobieta" {{ old('gender') == 'Kobieta' ? 'selected' : '' }}>Kobieta</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="height">Wzrost</label>
-            <select name="height" id="height" class="form-control">
-                @for ($i = 100; $i <= 220; $i++)
-                    <option value="{{ $i }}" {{ old('height') == $i ? 'selected' : '' }}>{{ $i }} cm</option>
-                @endfor
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="weight">Waga</label>
-            <select name="weight" id="weight" class="form-control">
-                @for ($i = 30; $i <= 200; $i++)
-                    <option value="{{ $i }}" {{ old('weight') == $i ? 'selected' : '' }}>{{ $i }} kg</option>
-                @endfor
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" value="{{ old('email') }}" required>
-        </div>
-
-        <div class="form-group">
-            <label for="age">Wiek</label>
-            <select name="age" id="age" class="form-control">
-                @for ($i = 13; $i <= 99; $i++)
-                    <option value="{{ $i }}" {{ old('age') == $i ? 'selected' : '' }}>{{ $i }}</option>
-                @endfor
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="physicalActivity">Poziom aktywności</label>
-            <select name="physicalActivity" id="physicalActivity">
-                <option {{ old('physicalActivity') == 'Brak treningów' ? 'selected' : '' }}>Brak treningów</option>
-                <option {{ old('physicalActivity') == 'Niska aktywność' ? 'selected' : '' }}>Niska aktywność</option>
-                <option {{ old('physicalActivity') == 'Średnia aktywność' ? 'selected' : '' }}>Średnia aktywność</option>
-                <option {{ old('physicalActivity') == 'Wysoka aktywność' ? 'selected' : '' }}>Wysoka aktywność</option>
-                <option {{ old('physicalActivity') == 'Bardzo wysoka aktywność' ? 'selected' : '' }}>Bardzo wysoka aktywność</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="goal">Cel diety</label>
-            <select name="goal" id="goal">
-                <option {{ old('goal') == 'Chcę schudnąć' ? 'selected' : '' }}>Chcę schudnąć</option>
-                <option {{ old('goal') == 'Chcę utrzymać wagę' ? 'selected' : '' }}>Chcę utrzymać wagę</option>
-                <option {{ old('goal') == 'Chcę przytyć' ? 'selected' : '' }}>Chcę przytyć</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
-            <input type="password" id="password_confirmation" name="password_confirmation" required>
-        </div>
-
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                <div>{{ $error }}</div>
-            @endforeach
-        @endif
-
-        <button type="submit">Register</button>
-    </form>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
-
 </html>
+
+<style>
+    #form-image {
+        background-image: url({{ asset('images/signup-user.jpg') }});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+
