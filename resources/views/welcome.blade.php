ac0d0d010d02f813074ccd7bd86b747851ce3eeb
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-11-03 11:39:12 +0100

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
 
 @include('includes.header')
 
-<div class="container-fluid">
+<div class="container-fluid gx-0">
+
     <div class="row min-vh-100 align-items-center" id="first-main">
         <div class="col-6 d-flex flex-column" id="first-sub-main">
             <p class="fs-2 fw-bold" id="first-sub-main-text-secondary">Zdrowie</p>
@@ -19,99 +20,116 @@
             </form>
         </div>
     </div>
-    <div class="row align-items-center" id="second-main" style="height: 20vh">
-        <div>
-            <b class="d-flex justify-content-center mb-3 fs-6" id="second-main-text-primary">OFERTA</b>
-            <b class="d-flex justify-content-center fs-3" id="second-main-text-secondary">Co nas wyróżnia?</b>
-        </div>
-    </div>
-    <div class="row align-items-center mb-5" id="third-main" style="height: 45vh">
-        <div class="col d-flex justify-content-center">
-            <div class="col-sm-5">
-                <img src="{{ asset('/images/grid1.svg') }}" class="img-fluid" alt="grid1">
-                <p class="d-flex justify-content-center mt-2 fw-medium">Składniki</p>
-                <p class="d-flex justify-content-center mt-2 text-center" id="third-main-subtext">Dostosujemy jadłospis
+
+    <div class="mt-5">
+
+        <div class="row mb-5 mt-5 pe-5 ps-5">
+            <div class="col-md-7 d-flex flex-column justify-content-center align-items-center text-center">
+                <p class="fw-normal" style="font-size:50px; letter-spacing: 1px">Składniki. <span
+                        class="text-body-secondary">Wszystko tak jak lubisz.</span></p>
+                <p class="lead">Dostosujemy jadłospis
                     do twoich potrzeb! Możesz wybrać do 5-ciu składników których nie lubisz, a my wykluczymy je z twoich
                     dań!</p>
             </div>
+            <div class="col-md-5 d-flex justify-content-center">
+                <img class="img-fluid mx-auto" src="{{ asset('images/grid1.svg') }}" alt="grid1" width="500"
+                     height="500">
+            </div>
         </div>
-        <div class="col d-flex justify-content-center">
-            <div class="col-sm-5">
-                <img src="{{ asset('/images/grid2.svg') }}" class="img-fluid" alt="grid2">
-                <p class="d-flex justify-content-center mt-2 fw-medium">Kalorie</p>
-                <p class="d-flex justify-content-center mt-2 text-center" id="third-main-subtext">Obliczymy dla Ciebie
+
+        <div class="row mb-5 mt-5 pe-5 ps-5">
+            <div class="col-md-7 order-md-2 d-flex flex-column justify-content-center align-items-center text-center">
+                <p class="fw-normal" style="font-size:50px; letter-spacing: 1px">Kalorie. <span
+                        class="text-body-secondary">Od czegoś trzeba zacząć.</span></p>
+                <p class="lead">Obliczymy dla Ciebie
                     zapotrzebowanie kaloryczne według Twoich potrzeb, niezależnie czy uprawiasz bardzo dużo sportu, czy
-                    prowadzisz biurowy styl żyica!</p>
+                    prowadzisz biurowy styl życia.</p>
+            </div>
+            <div class="col-md-5 d-flex justify-content-center">
+                <img class="img-fluid mx-auto" src="{{ asset('images/grid2.svg') }}" alt="grid2" width="500"
+                     height="500">
             </div>
         </div>
-        <div class="col d-flex justify-content-center">
-            <div class="col-sm-5">
-                <img src="{{ asset('/images/grid3.svg') }}" class="img-fluid" alt="grid3">
-                <p class="d-flex justify-content-center mt-2 fw-medium">Zdrowie</p>
-                <p class="d-flex justify-content-center mt-2 text-center" id="third-main-subtext">Będziemy monitorować
-                    twoje dane, dzięki którym będziesz mieć wgląd do swojej historii zdrowia!</p>
+
+
+        <div class="row mb-5 mt-5 pe-5 ps-5">
+            <div class="col-md-7 d-flex flex-column justify-content-center align-items-center text-center">
+                <p class="fw-normal" style="font-size:50px; letter-spacing: 1px">Zdrowie. <span
+                        class="text-body-secondary">Czyli to co najważniejsze.</span></p>
+                <p class="lead">Będziemy monitorować
+                    twoje dane, dzięki którym będziesz mieć wgląd do swojej historii zdrowia.</p>
+            </div>
+            <div class="col-md-5 d-flex justify-content-center">
+                <img class="img-fluid mx-auto" src="{{ asset('images/grid3.svg') }}" alt="grid3" width="500"
+                     height="500">
             </div>
         </div>
-    </div>
-    <div class="row" id="fourth-main">
-        <div class="col p-0">
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                            class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner text-center">
-                    <div class="carousel-item active" id="car1">
-                        <div class="d-flex h-100 align-items-center justify-content-center">
-                            <div class="d-flex align-items-center justify-content-center p-5" id="center">Twoje
-                                zdrowie, nasza pasja: indywidualne przepisy, profesjonalne doradztwo!<br> Oferujemy nie
+
+        <div id="myCarousel" class="carousel slide mb-5 mt-5" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"
+                        aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"
+                        class=""></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"
+                        class="" aria-current="true"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="col-md-12" id="carousel1">
+                    </div>
+                    <div class="container">
+                        <div class="carousel-caption text-start">
+                            <h1>Twoje
+                                zdrowie, nasza pasja: indywidualne przepisy, profesjonalne doradztwo!</h1>
+                            <p class="opacity-75"> Oferujemy nie
                                 tylko smaczne dania, ale także specjalnie układane przepisy
                                 przez doświadczonych dietetyków. Odkryj smak zdrowego życia z naszymi unikalnymi
                                 recepturami, dostosowanymi do Twoich potrzeb – z nami dieta może być zarówno smaczna,
-                                jak i zdrowa.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item" id="car2">
-                        <div class="d-flex h-100 align-items-center justify-content-center">
-                            <div class="d-flex align-items-center justify-content-center pe-5 ps-5" id="center">
-                                Odkryj
-                                swoją najlepszą wersję z nami! <br> Foodie fix nie tylko pomaga Ci osiągnąć
-                                swoje cele wagowe, ale także utrzymać zdrową wagę przez długi czas. Dzięki naszemu
-                                doświadczeniu i pasji pomożemy Ci zrzucić zbędne kilogramy lub osiągnąć wymarzoną
-                                sylwetkę. Zaufaj nam na swojej drodze do lepszej wersji siebie!
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item" id="car3">
-                        <div class="d-flex h-100 align-items-center justify-content-center">
-                            <div class="d-flex align-items-center justify-content-center pe-5 ps-5" id="center">
-                                Osiągnij swoje cele dzięki precyzyjnemu podejściu! <br> Oferujemy nie tylko
-                                wyliczanie kalorii, ale również kompleksowe wsparcie w Twojej drodze do zdrowia. Dzięki
-                                dokładnemu monitorowaniu kaloryczności posiłków, możemy dostosować Twoją dietę do Twoich
-                                indywidualnych potrzeb. Odpowiednio obliczone kalorie - to klucz do osiągnięcia
-                                wymarzonej sylwetki i pełnego energii życia!
-                            </div>
+                                jak i zdrowa.</p>
                         </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                        data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                        data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <div class="carousel-item">
+                    <div class="col-md-12" id="carousel2">
+                    </div>
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Odkryj swoją najlepszą wersję z nami!</h1>
+                            <p>Foodie fix nie tylko pomaga Ci osiągnąć
+                                swoje cele wagowe, ale także utrzymać zdrową wagę przez długi czas. Dzięki naszemu
+                                doświadczeniu i pasji pomożemy Ci zrzucić zbędne kilogramy lub osiągnąć wymarzoną
+                                sylwetkę. Zaufaj nam na swojej drodze do lepszej wersji siebie!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-12" id="carousel3">
+                    </div>
+                    <div class="container">
+                        <div class="carousel-caption text-end">
+                            <h1>Osiągnij swoje cele dzięki precyzyjnemu podejściu!</h1>
+                            <p>Oferujemy nie tylko
+                                wyliczanie kalorii, ale również kompleksowe wsparcie w Twojej drodze do zdrowia. Dzięki
+                                dokładnemu monitorowaniu kaloryczności posiłków, możemy dostosować Twoją dietę do Twoich
+                                indywidualnych potrzeb. Odpowiednio obliczone kalorie - to klucz do osiągnięcia
+                                wymarzonej sylwetki i pełnego energii życia!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
+
     </div>
+
 </div>
 
 @include('includes.footer')
@@ -123,41 +141,26 @@
 </html>
 
 <style>
-    #car1 {
-        height: 50vh;
+    #carousel1 {
+        height: 65vh;
         background-size: cover;
         background-position: center center;
         background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url({{ asset('images/carousel1.jpg') }});
     }
 
-    #car2 {
-        height: 50vh;
+    #carousel2 {
+        height: 65vh;
         background-size: cover;
         background-position: center center;
         background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url({{ asset('images/carousel2.jpg') }});
     }
 
-    #car3 {
-        height: 50vh;
+    #carousel3 {
+        height: 65vh;
         background-size: cover;
         background-position: center center;
         background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url({{ asset('images/carousel3.jpg') }});
     }
 
-    #center {
-        margin: 0;
-        width: 50%;
-        height: 300px;
-        background-color: rgba(255, 255, 255, .9);
-        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .2);
-        word-wrap: break-word;
-    }
 
-    #second-main-text-primary {
-        color: #76c893;
-    }
-
-    #third-main-subtext {
-        font-size: 14px;
-    }
 </style>
