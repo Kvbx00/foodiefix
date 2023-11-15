d2c586c6adde47daa41a392b44bc25f7634c35b0
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-11-15 11:15:20 +0100

    admin login view

--- resources/views/administratorAuth/login.blade.php
+++ resources/views/administratorAuth/login.blade.php
@@ -1,30 +1,69 @@
 <!doctype html>
-<html>
+<html lang="pl">
 
-<head>
-    <meta charset="UTF-8">
-    <title>FoodieFix</title>
-</head>
+@include('includes.head')
 
 <body>
-<form method="POST" action="{{ route('admin.login') }}">
-    @csrf
 
-    <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<div class="container">
+    @include('includes.error')
 
-    <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <div class="row justify-content-center align-items-center" style="height:100vh">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.login') }}">
+                        @csrf
+                        <div class="form-floating my-3">
+                            <img src="{{ asset('images/logo-admin.png') }}" alt="logo" width="50" height="44">
+                        </div>
+                        <div class="form-floating my-3">
+                            <input type="email" id="floatingEmail"
+                                   class="form-control @error('email') is-invalid @enderror" name="email"
+                                   value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <label for="floatingEmail">Adres email</label>
+                        </div>
+                        <div class="form-floating my-3">
+                            <input type="password" id="floatingPassword"
+                                   class="form-control @error('password') is-invalid @enderror" name="password" required
+                                   autocomplete="current-password">
+                            <label for="floatingPassword">Hasło</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Zaloguj</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div>{{ $error }}</div>
-        @endforeach
-    @endif
+</div>
 
-    <button type="submit">
-        {{ ('Zaloguj') }}
-    </button>
-
-</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
-
 </html>
+
+<style>
+    body{
+        background: rgb(56,56,56);
+        background: linear-gradient(90deg, rgba(56,56,56,1) 0%, rgba(0,0,0,1) 50%, rgba(56,56,56,1) 100%);
+    }
+
+    .card{
+        background-color: #000000;
+        border: 1px solid #FFFFFF;
+        box-shadow: 0 4px 16px 0 rgba(255, 255, 255, .5);
+    }
+
+    .btn{
+        background-color: #000000;
+        border: 1px solid #FFFFFF;
+    }
+
+    .btn:hover{
+        background-color: #FFFFFF;
+        color: #000000;
+        border: 1px solid #000000;
+    }
+</style>
