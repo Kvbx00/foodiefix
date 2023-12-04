cb049c51f8575d0284079319e22f17d6cf115ba4
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-12-04 11:33:44 +0100

    removed old jquery

--- resources/views/administrator/editUserMenuMeal.blade.php
+++ resources/views/administrator/editUserMenuMeal.blade.php
@@ -57,7 +57,6 @@
         crossorigin="anonymous"></script>
 </body>
 </html>
-<script src="{{ asset('jquery/jquery.min.js') }}"></script>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
